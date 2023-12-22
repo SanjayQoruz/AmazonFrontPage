2625897b33c953dcafb30a374cce8ab196113056
SanjayQoruz <sanjay@qoruz.com>
2023-12-22 10:52:24 +0530

    .

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -18,17 +18,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget{
-  const MyHomePage({super.key});
-
-     @override
-     Widget build (BuildContext context){
-         return const SafeArea(child:Scaffold(
-            body:City(),
-         ));
-     }
-}
-
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
